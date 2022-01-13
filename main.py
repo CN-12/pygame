8efab68146d8e71d46d7d7f7f46ad27c57b698a7
CN-12 <80306141+CN-12@users.noreply.github.com>
2022-01-13 10:34:04 +0000

    체인지

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import keyboard
 import os
+import time
+
 def check(k):
     if (k == 9) | (k == 0) :
-        print("시발년아")
+        print("안돼요")
         return 0
     return 1
 
@@ -10,20 +12,27 @@ i,j = 0, 0
 pan = [[0 for i in range(0, 10)] for j in range(0, 10)]
 pan[0][0] = "1"
 while True :
-  for i in range(0, 10):
-    for j in range(0, 10):
-      print(pan[i][j], end="")
-    print("\n")
-  if keyboard.is_pressed('up'):
+    for i in range(0, 10):
+        for j in range(0, 10):
+            print(pan[i][j], end="")
+        print("\n")
+    if(keyboard.is_pressed('up')):
         if check(j):
-          j = j + 1
-  if keyboard.is_pressed('down'):
+            j = j + 1
+            time.sleep(0.1)
+    if (keyboard.is_pressed('down')):
         if check(j):
-          j = j - 1
-  if keyboard.is_pressed('left'):
+            j = j - 1
+    time.sleep(0.1)
+
+  if (keyboard.is_pressed('left')):
         if check(i):
           i = i - 1
-  if keyboard.is_pressed('right'):
-        if check(i):
-          i = i + 1
+        time.sleep(0.1)
+
+  if (keyboard.is_pressed('right')):
+    if check(i):
+        i = i + 1
+        time.sleep(0.1)
+
   os.system('clear')
